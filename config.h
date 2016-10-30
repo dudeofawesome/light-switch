32664a240b1817e42bb19a96130c83b263a554e0
Louis Orleans <louis@orleans.io>
2016-10-29 21:40:45 -0700

    Adding sample config.h file

---
+++ config.h
@@ -0,0 +1,6 @@
+//  your network SSID (name)
+const char SSID[] = "YOUR SSID HERE";
+// your network password
+const char PASSWORD[] = "YOUR PASSWORD HERE";
+// LifX bearer token
+const char LIFX_BEARER_TOKEN_HEADER[] = "Authorization: Bearer YOUR_LIFX_API_TOKEN_HERE";
